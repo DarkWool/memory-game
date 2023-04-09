0e9abb82071ef974d468239a47c414932cbc412a
DarkWool <cooper27@live.com.mx>
2023-04-09 14:09:16 -0600

    Set up a basic game loop with score
    When the same card (color) has already been clicked in the past the
    game displays an alert to inform the user that the game is over and it
    resets the score.
    Each time a card is clicked and the game has not been lost the cards
    are shuffled to confuse the player.
    
    Create colorsData.js to store the information about the different
    colors that can be displayed in game with placeholder data that soon
    will be replaced.

---
+++ src/colorsData.js
@@ -0,0 +1,29 @@
+const colorsData = [
+  {
+    id: "c1",
+    name: "Lapis Lazuli",
+    styles: "lazuli",
+  },
+  {
+    id: "c2",
+    name: "Emerald",
+    styles: "emerald",
+  },
+  {
+    id: "c3",
+    name: "Purple",
+    styles: "purple",
+  },
+  {
+    id: "c4",
+    name: "Viva Magenta",
+    styles: "",
+  },
+  {
+    id: "c5",
+    name: "Orchid",
+    styles: "orchid",
+  },
+];
+
+export { colorsData };
--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -1,9 +1,61 @@
 import { useState } from "react";
+import { ColorCard } from "./ColorCard";
+import { colorsData } from "../colorsData";
 
-export function GameScreen() {
-  function handleColorClick(e) {
-    console.log(e);
+function shuffle(array) {
+  let shuffledArr = [...array];
+
+  for (let i = shuffledArr.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [shuffledArr[i], shuffledArr[j]] = [shuffledArr[j], shuffledArr[i]];
   }
 
-  return <section></section>;
+  return shuffledArr;
+}
+
+export function GameScreen() {
+  const [score, setScore] = useState(0);
+  const [clickedColors, setClickedColors] = useState([]);
+
+  console.log(clickedColors);
+
+  function handleColorClick(id, color) {
+    console.log(`CLICKED ON: ${color}`);
+
+    if (clickedColors.includes(id)) {
+      alert("Game over, item clicked twice");
+      return resetGame();
+    }
+
+    setScore((s) => s + 1);
+    setClickedColors([...clickedColors, id]);
+  }
+
+  function resetGame() {
+    setScore(0);
+    setClickedColors([]);
+  }
+
+  const shuffledColors = shuffle(colorsData);
+
+  return (
+    <>
+      <header>
+        <p>
+          Score: <span>{score}</span>
+        </p>
+      </header>
+      <section className="game_cards-board">
+        {shuffledColors.map((color) => {
+          return (
+            <ColorCard
+              key={color.id}
+              color={color}
+              onClick={handleColorClick}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
 }
