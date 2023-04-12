1cf432db8abb8573bf28d7da8b2569797548f6b5
DarkWool <cooper27@live.com.mx>
2023-04-12 15:06:14 -0600

    Add scale animation each time the score increments

--- src/components/GameHeader.jsx
+++ src/components/GameHeader.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useRef } from "react";
+
 export function GameHeader({ bestScore, score, level }) {
+  const scoreRef = useRef(null);
+
+  useEffect(() => {
+    let timer;
+    if (score !== 0) {
+      const node = scoreRef.current;
+      node.classList.add("scale");
+      setTimeout(() => node.classList.remove("scale"), 220);
+    }
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [score]);
+
   return (
     <header className="game_header">
       <div>
         <span className="game_header-best">Best: {bestScore}</span>
       </div>
+
       <div className="game_header-middle">
         <h1 className="menu_title game_header-title">
           Remember <br />
@@ -14,8 +32,11 @@ export function GameHeader({ bestScore, score, level }) {
           <span className="fw-700">Level:&nbsp;</span> {level}
         </p>
       </div>
+
       <div className="game_header-right">
-        <span className="btn-primary d-inline-block">Score: {score}</span>
+        <span className="btn-primary d-inline-block game_score" ref={scoreRef}>
+          Score: {score}
+        </span>
       </div>
     </header>
   );
