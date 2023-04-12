995fea45e0f4c228f75f04eee960497041a48036
DarkWool <cooper27@live.com.mx>
2023-04-12 15:12:50 -0600

    Create MenuScreen, Button and Heading components
    A basic state was added to the App component to go from the Menu to the
    Game screen.
    Some styles were also added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from "react";
+import { MenuScreen } from "./components/MenuScreen";
 import { GameScreen } from "./components/GameScreen";
 
 function App() {
+  const [isGameStarted, setIsGameStarted] = useState(false);
+
+  function handleGameStart() {
+    setIsGameStarted(true);
+  }
+
   return (
     <>
-      <GameScreen />
+      {isGameStarted ? (
+        <GameScreen />
+      ) : (
+        <MenuScreen onGameStart={handleGameStart} />
+      )}
     </>
   );
 }
