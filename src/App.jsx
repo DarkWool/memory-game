193eb705a3d2d0db7ff03d9c5be2934a2a25bca9
DarkWool <cooper27@live.com.mx>
2023-04-29 11:58:04 -0600

    Fix a prop name on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
       {gameMode ? (
-        <GameScreen mode={gameMode} />
+        <GameScreen gameMode={gameMode} />
       ) : (
         <MenuScreen onGameStart={handleGameStart} />
       )}
